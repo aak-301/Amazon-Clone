bf6b03e8a14eb2c0ac75e4669290575dcb355867
aakash-301 <aakashshivanshu5@gmail.com>
2023-06-15 15:36:02 +0530

    added the db connections

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const authRouter = require("./routes/auth");
 
+// init
 const app = express();
-
 const PORT = 3000;
+const DB =
+  "mongodb+srv://aakash:aakas_301@cluster0.fzs30ay.mongodb.net/?retryWrites=true&w=majority";
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+//middleware
+app.use(express.json());
+app.use(authRouter);
+
+//connection
+//  username: aakash
+// password:aakas_301
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Db Connection succesfull");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Listening on port ${PORT}`);
