ae9c96b649e13a5e57a740c1529c1c3e46fe40c5
aakash-301 <aakashshivanshu5@gmail.com>
2023-06-15 15:39:31 +0530

    added routes for home screen an bottom navigation

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,9 @@
 import 'package:amazonclone/features/auth/screens/auth_screen.dart';
 import 'package:flutter/material.dart';
 
+import 'common/widgets/bottom_bar.dart';
+import 'features/home/screens/home_screen.dart';
+
 Route<dynamic> generateRoute(RouteSettings routeSettings) {
   switch (routeSettings.name) {
     case AuthScreen.routeName:
@@ -8,6 +11,23 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
         settings: routeSettings,
         builder: ((_) {
           return const AuthScreen();
+        }),
+      );
+
+    case HomeScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: ((_) {
+          return const HomeScreen();
+        }),
+      );
+
+      
+    case BottomBar.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: ((_) {
+          return const BottomBar();
         }),
       );
 
