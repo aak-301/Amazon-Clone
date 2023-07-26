ac9eee9de25ccf03f94adee590fce590cd9bfbf5
aakash-301 <aakashshivanshu5@gmail.com>
2023-07-26 11:56:08 +0530

    product Schema in mongo

---
+++ server/models/product.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+    trim: true,
+  },
+  description: {
+    required: true,
+    type: String,
+    trim: true,
+  },
+  images: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  // ratings
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
