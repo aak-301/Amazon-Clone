56ed287dc97b3b049dc816d01beea99b74563502
aakash-301 <aakashshivanshu5@gmail.com>
2023-07-27 17:43:58 +0530

    small fix for rating the product

--- lib/features/search/widget/serched_product.dart
+++ lib/features/search/widget/serched_product.dart
@@ -51,7 +51,7 @@ class SearchedProduct extends StatelessWidget {
                     width: 235,
                     padding: const EdgeInsets.only(left: 10, top: 5),
                     child: Stars(
-                      rating: 3,
+                      rating: avgRating,
                     ),
                   ),
                   Container(
